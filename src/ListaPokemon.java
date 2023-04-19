cbfcdfcc80a6ab05f9201197423b27c3094b89e9
usr2775 <usr2775@salesianos-lacuesta.net>
2023-04-19 12:46:27 +0100

    feat: creado de constructores

--- src/ListaPokemon.java
+++ src/ListaPokemon.java
@@ -3,5 +3,8 @@ public class ListaPokemon {
     private Pokemon[] pokemonList;
     private int size;
 
-
+    public ListaPokemon() {
+        pokemonList = new Pokemon[1];
+        size = 0;
+    }
 }
