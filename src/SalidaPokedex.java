0b75a15bdb28ad6b2c02521cf2a33c638c733a7b
usr2775 <usr2775@salesianos-lacuesta.net>
2023-04-19 12:58:59 +0100

    feat: creado de objetos

--- src/SalidaPokedex.java
+++ src/SalidaPokedex.java
@@ -1,5 +1,23 @@
 public class SalidaPokedex {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            ListaPokemon avistados = new ListaPokemon();
+            ListaPokemon capturados = new ListaPokemon();
+    
+            System.out.println("Bienvenido a la Pokédex personalizada!");
+    
+            boolean salir = false;
+            while (!salir) {
+                System.out.println("a) Añadir un pokemon avistado");
+                System.out.println("b) Añadir avistamiento legendario");
+                System.out.println("c) Pasar a capturados");
+                System.out.println("d) Mostrar lista de avistados");
+                System.out.println("e) Mostrar lista de capturados");
+                System.out.println("f) Cerrar la pokedex");
+    
+                String opcion = scanner.nextLine();
     }
+
+    
 }
