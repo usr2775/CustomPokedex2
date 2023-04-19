0d89f877d640bd3de61feef8bb9d10f04d795f67
usr2775 <usr2775@salesianos-lacuesta.net>
2023-04-19 12:17:43 +0100

    feat: creado de los constructores

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -5,4 +5,12 @@ public class Pokemon {
     private double peso;
     private String descripcion;
 
+
+    public Pokemon(String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.altura = altura;
+        this.peso = peso;
+        this.descripcion = descripcion;
+    }
 }
