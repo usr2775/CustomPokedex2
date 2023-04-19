946b8dbf70e8d50ff9382c449efe26312768c383
usr2775 <usr2775@salesianos-lacuesta.net>
2023-04-19 12:47:18 +0100

    feat: añadido metodo void addPokemon

--- src/ListaPokemon.java
+++ src/ListaPokemon.java
@@ -7,4 +7,18 @@ public class ListaPokemon {
         pokemonList = new Pokemon[1];
         size = 0;
     }
+
+    public void addPokemon(Pokemon pokemon) {
+        if (size == pokemonList.length) {
+            Pokemon[] temp = new Pokemon[pokemonList.length * 2];
+            for (int i = 0; i < pokemonList.length; i++) {
+                temp[i] = pokemonList[i];
+            }
+            pokemonList = temp;
+        }
+        pokemonList[size] = pokemon;
+        size++;
+        System.out.println("El pokemon " + pokemon.getNombre() + " ha sido añadido a la lista.");
+   
+        
 }
