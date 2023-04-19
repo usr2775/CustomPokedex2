8d1e64712c339835f17d7c539c0c64032be84b40
usr2775 <usr2775@salesianos-lacuesta.net>
2023-04-19 12:37:35 +0100

    feat: creado clase pokemonL con su atributo y su contructor

---
+++ src/PokemonLegendario.java
@@ -0,0 +1,11 @@
+public class PokemonLegendario extends Pokemon {
+    private String localizacion;
+
+    public PokemonLegendario(String nombre, String tipo, String descripcion) {
+        super(nombre, tipo);
+        this.localizacion = localizacion;
+    }
+
+
+    
+}
